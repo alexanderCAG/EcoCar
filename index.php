867483c7c59a3db9e9426e577928717fa12ad834
alexanderCAG <61508790+alexanderCAG@users.noreply.github.com>
2022-01-17 11:25:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
-    <<<<<<< HEAD
-    test
-    
-    bla bla bla
-    >>>>>>> 6e6aea592ef8929241cb42891eccd41cb6a6b0cc
-    =======
-bla bla
 
-bla bla bla
->>>>>>> 6e6aea592ef8929241cb42891eccd41cb6a6b0cc
