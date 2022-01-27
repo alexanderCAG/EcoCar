53854d91806cea2026c8adec229f5b4854a2e95c
alexanderCAG <61508790+alexanderCAG@users.noreply.github.com>
2022-01-28 00:31:32 +0100

    fin animation marque (accueil)

--- Style/general.js
+++ Style/general.js
@@ -4,13 +4,17 @@ $(window).scroll(function(){
     var trait_vert = document.getElementById("image_fond_accueil");
     if($(this).scrollTop() > 1330){
         // alert('ok');
-        let i = 0;
+        var i = 0;
         for(i=0;i<30;i++){
             let nbr1 = 40 + i;
             let nbr2 = 35 + i;
             let nbr3 = 30 + i;
-            trait_vert.style.clipPath = "inset(0 60%, 0% 100%, 0 70%, 0 70%, 100% "+nbr1+"%, 100% 100%, 0 100%, 0 65%, 100% "+nbr2+"%, 100% "+nbr3+"%)" 
+            trait_vert.style.clipPath = "polygon(0 60%, 0% 100%, 0 70%, 0 70%, 100% "+nbr1+"%, 100% 100%, 0 100%, 0 65%, 100% "+nbr2+"%, 100% "+nbr3+"%)";
+            // sleep(500);
+            trait_vert.style.transition = "clip-path 1s ease 0s";
         }
+    }else{
+        trait_vert.style.clipPath = "polygon(0 60%, 0% 100%, 0 70%, 0 70%, 100% 40%, 100% 100%, 0 100%, 0 65%, 100% 35%, 100% 30%)";
     }
 
 });
