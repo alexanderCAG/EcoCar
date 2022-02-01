937a2b5162724768c133259398cd42b083cd780b
alexanderCAG <61508790+alexanderCAG@users.noreply.github.com>
2022-02-01 20:17:03 +0100

    admin1 contenu

--- Acheteur/index.php
+++ Acheteur/index.php
@@ -20,20 +20,66 @@
             <a href="" class="color_white text-decoration-none"><li class="liste_sidebar">Mon compte</li></a>
             <a href="" class="color_white text-decoration-none"><li class="liste_sidebar">Deconnexion</li></a>
         </ul>
-        <!-- <ul>
-            <li class="sidebar_liste_menu"><a href="boulangerie.php" class="nom_menu_sidebar deco_none text-uppercase">Boulangerie</a></li><br>
-            <li class="sidebar_liste_menu"><a href="gateau.php" class="nom_menu_sidebar deco_none text-uppercase">Gateaux</a></li><br>
-            <li class="sidebar_liste_menu"><a href="entremet.php" class="nom_menu_sidebar deco_none text-uppercase">Entremets</a></li><br>
-        </ul> -->
     </nav>
 
     <!-- Contenue -->
-    <div class="contenue">
+    <div class="contenue bg_gray3">
 
-        <div class="div_accueil_color bg_green1"> </div>
+        <section>
+            <div class="div_accueil_color bg_green1">
+                <div class="row">
+                    <div class="col-8"></div>
+                    <div class="col-4">
+                        <a href="">
+                            <div class="bg_gray2 div_ajout_admin2">
+                                <small><p>Ajouter une véhicule</p></small>
+                            </div>
+                        </a>
+                        <a href="">
+                            <div class="bg_gray2 div_notif_admin2">
+                                <i class="fas fa-user-friends icone_notif2"></i>
+                                <span>50</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-8 div_gauche_admin">
+                    <div class="contenu_gauche1_admin shadow bg_white">
+                        <img src="../Image/voiture_test.jpg" alt="last vehicule" class="img_last_save">
+                        <div class="gauche1_admin_texteUp text-uppercase titre">
+                            <h4>Dernier véhicule enregistré</h4>
+                            <h6>Mettre du texte ici</h6>
+                            <p class="prix_last_save"><span>500,50</span>€/Mois</p>
+                        </div>
+                        <p class="texte_last_save">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate impedit iure ad cumque iusto reiciendis explicabo expedita harum praesentium nihil necessitatibus enim placeat neque debitis, tenetur nisi aliquam eaque! Soluta?</p>
+                        <div class="info_last_save text-uppercase">
+                            <i class="bi bi-shield-fill-check fa-2x color_green1"></i>
+                            <small><span>Garantie 24 mois | 2021 | 14 137 km</span></small>
+                        </div>
+                    </div>
+                    <div class="mt-5 contenu_gauche1_admin shadow">
+                        <br><br><br><br><br><br><br>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="row">
+                        <div class="col-12 contenu_droit1_admin shadow">
+                            <br><br><br><br><br><br><br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
 
         <!-- Presentation categorie -->
-        <div class="container">
+        <!-- <div class="container">
                 <div class="row presentation_categorie">
                     <div class="col-6">
                         <p class="texte_presentation_categorie text-uppercase">Boulangerie</p>
@@ -54,7 +100,7 @@
                 </div>
             </div>
 
-        </div>
+        </div> -->
 
 </div>
 
