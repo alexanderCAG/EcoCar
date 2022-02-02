44f8f1a15d093557afb83b7c4f22725569fcfbbf
alexanderCAG <61508790+alexanderCAG@users.noreply.github.com>
2022-02-02 10:07:55 +0100

    stat admin accueil

--- Acheteur/index.php
+++ Acheteur/index.php
@@ -79,6 +79,7 @@
                         <!-- Partie 2 -->
                         <div class="mt-4 contenu_gauche2_admin shadow bg_white container mx-2">
                             <h4 class="text-uppercase mx-4 pt-4 t_bold titre">Total de location des véhicules</h4>
+                            
                             <div class="row align-items-center mt-5">
                                 <div class="col-9">
                                     <p class="mx-4">Contrat de location signés</p>
@@ -103,12 +104,86 @@
                                     </div>
                                 </div>
                             </div>
+
+                            <div class="row pt-3 pb-3" style="margin-top: 40px">
+                                <div class="col-md-12">
+                                    <div class="border-0 rounded-0 text-uppercase" style="height: 500px!important">
+                                        <!-- <br>
+                                        <div class="row">
+                                            <div class="col-12 text-center">
+                                                <span class="m-2 pl-4 p-2  text-light" style="background-color:#5499C7">Pillow</span>
+                                                <span class="m-2 pl-4 p-2  text-light" style="background-color:#F39C12">Decoraton</span>
+                                                <span class="m-2 pl-4 p-2  text-light" style="background-color:#52BE80">Sheet</span>
+                                                <span class="m-2 pl-4 p-2  text-light" style="background-color:#A569BD">T-shirt</span>
+                                                <span class="m-2 pl-4 p-2  text-light" style="background-color:#B03A2E">Sweat-shirt</span>
+                                                <span class="m-2 pl-4 p-2  text-light" style="background-color:#935116">Shoes</span>
+                                            </div>
+                                        </div> -->
+                                
+                                        <div class="container contain_bar h-100 pt-5" style="margin-left:2%;"> 
+                                            <!-- ##### Stat pillow #######-->
+
+                                            <div class="progress progress-bar-vertical h-75 bg-transparent" style="width:120px">
+                                                <div class="bar1_progress progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="height: 50%; background-color:#c0dfcd">
+                                                    <span class="">50%</span>
+                                                </div>
+                                                <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="height: 80%; background-color:#79bb94">
+                                                    <span class="">80%</span>
+                                                </div>
+                                            </div>
+
+                                            <!-- ##### Stat decoration #######-->
+
+                                            <div class="progress progress-bar-vertical h-75 bg-light shadow-lg" style="width:120px">
+                                                <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="height: 70%; background-color: #F39C12;">
+                                                    <span class="">70%</span>
+                                                </div>
+                                            </div>
+
+                                            <!-- ##### Stat sheet #######-->
+
+                                            <div class="progress progress-bar-vertical h-75 bg-light shadow-lg" style="width:120px">
+                                                <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="height: 20%; background-color: #52BE80;">
+                                                    <span class="">20%</span>
+                                                </div>
+                                            </div>
+
+                                            <!-- ##### Stat Tshirt #######-->
+
+                                            <div class="progress progress-bar-vertical h-75 bg-light shadow-lg" style="width:120px">
+                                                <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="height: 65%; background-color: #A569BD;">
+                                                    <span class="">65%</span>
+                                                </div>
+                                            </div>
+
+                                            <!-- ##### Stat Sweat-shirt #######-->
+
+                                            <div class="progress progress-bar-vertical h-75 bg-light shadow-lg" style="width:120px">
+                                                <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="height: 85%; background-color: #B03A2E;">
+                                                    <span class="">85%</span>
+                                                </div>
+                                            </div>
+
+                                            <!-- ##### Stat Shoes #######-->
+
+                                            <div class="progress progress-bar-vertical h-75 bg-light shadow-lg" style="width:120px">
+                                                <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="height: 45%; background-color: #935116;">
+                                                    <span class="">45%</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+  
+                            </div>
                         </div>
+
+                        <!-- Partie 3 -->
 
                     </div>
 
                     <!-- Droit -->
-                    <div class="col-3">
+                    <div class="col-3 bg_green3">
 
                         <!-- Partie 1 -->
                         <div class="contenu_droit1_admin shadow bg_white">
