25bc45cdabee62a5c7b0a07b7ad063329989cd2e
alexanderCAG <61508790+alexanderCAG@users.noreply.github.com>
2022-02-18 16:48:15 +0100

    Update carousel.js

--- Style/carousel.js
+++ Style/carousel.js
@@ -1,9 +1,7 @@
 
 
 $(document).ready(function(){
-    // boucle();
-    // document.getElementById('check_inscri').checked=true;
-
+    boucle();
 });
 
 jQuery(document).ready(function(){
@@ -32,14 +30,11 @@ function ViewPromo2(){
     $('.promo_tot2').show();
 }
 
-// function boucle(){
-//     let compteur_boucle = 0;
-//     do{
-//         setTimeout(function(){
-//             setTimeout(function(){$('#bestSeller_tot').hide();$('#promo_tot').show();}, 2000);
-//             setTimeout(function(){$('#promo_tot').hide();$('#bestSeller_tot').show();}, 2000);
-//             compteur_boucle++;
-//         }, 2000);
+function boucle(){
+    setTimeout(function(){
+        setTimeout(function(){$('#bestSeller_tot').hide();$('#promo_tot2').hide();$('#promo_tot1').show();}, 2000);
+        setTimeout(function(){$('#promo_tot1').hide();$('#promo_tot2').hide();$('#bestSeller_tot').show();}, 2000);
+        setTimeout(function(){$('#promo_tot1').hide();$('#bestSeller_tot').hide();$('#promo_tot2').show();}, 2000);
+    }, 2000);
         
-//     }while(compteur_boucle==10);
-// }
+}
