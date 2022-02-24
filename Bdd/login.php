4fa98f34a1b250ac6697016ee9510743ab22a4ec
alexanderCAG <61508790+alexanderCAG@users.noreply.github.com>
2022-02-24 11:53:21 +0100

    Update login.php

--- Bdd/login.php
+++ Bdd/login.php
@@ -8,8 +8,8 @@
         $usermail=$_POST['username'];
         $password=$_POST['password'];
 
-        $acheteurCon = mysqli_query($con, "select * from inscription where mdp='$password' and (email='$usermail' or nom='$usermail') and administrateur=0 ");
-		$adminCon = mysqli_query($con, "select * from inscription where mdp='$password' and (email='$usermail' or nom='$usermail') and administrateur=1 ");
+        $acheteurCon = mysqli_query($con, "select * from inscription where mdp='$password' and email='$usermail' and administrateur=0 ");
+		$adminCon = mysqli_query($con, "select * from inscription where mdp='$password' and email='$usermail' and administrateur=1 ");
 
         // INSERT INTO buyer (lastname,firstname,phone,email,passworde,adress1,adress2,city,zip_code,country) 
 		// 		VALUES ('$nom_acheteur','$prenom_acheteur','$telephone_acheteur','$mail_acheteur','$mdp_acheteur','$adresse1_acheteur','$adresse2_acheteur','$ville_acheteur','$codepostal_acheteur','$pays_acheteur')"
