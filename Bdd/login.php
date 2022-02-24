9836b86fea71b0c6a1471692ddae6c197312058f
MarineAmr <73342447+MarineAmr@users.noreply.github.com>
2022-02-24 12:45:06 +0100

    php inscription

--- Bdd/login.php
+++ Bdd/login.php
@@ -11,9 +11,6 @@
         $acheteurCon = mysqli_query($con, "insert * from inscription where mdp='$password' and (email='$usermail' or nom='$usermail') and administrateur=0 ");
 		$adminCon = mysqli_query($con, "select * from inscription where mdp='$password' and (email='$usermail' or nom='$usermail') and administrateur=1 ");
 
-        // INSERT INTO buyer (lastname,firstname,phone,email,passworde,adress1,adress2,city,zip_code,country) 
-		// 		VALUES ('$nom_acheteur','$prenom_acheteur','$telephone_acheteur','$mail_acheteur','$mdp_acheteur','$adresse1_acheteur','$adresse2_acheteur','$ville_acheteur','$codepostal_acheteur','$pays_acheteur')"
-
         // verifie the different connection
         if (mysqli_num_rows($acheteurCon) != 0){
             $_SESSION['emailUser']=$usermail;
@@ -26,5 +23,31 @@
             echo "<script language='javascript' type='text/javascript'> location.href='../Base/connexion.php' </script>";
         }
     }
+
+    if (isset($_POST['inscription'])){
+
+        include("cnx.php");
+        session_start();
+        $usernom=$_POST['nom'];
+        $userprenom=$_POST['prenom'];
+        $userphone=$_POST['telephone'];
+        $userdate=$_POST['date_permis'];
+        $useremail=$_POST['email'];
+        $usermdp=$_POST['mdp'];
+        // $useradmin='0';
+
+        $userInscription = mysqli_query($con,"INSERT INTO inscription (nom, prenom, email, phone, datePermis, mdp, administrateur) 
+                            VALUES ('$usernom', '$userprenom', '$userphone', '$userdate', '$useremail', '$usermdp', '0')");
+
+        // redirection vers la page connexion
+        // if($con->query($userInscription)===true){
+            echo "<script language='javascript' type='text/javascript'> location.href='../Base/connexion.php'</script>";
+        // }
+        // else{
+        //     echo "<script language='javascript' type='text/javascript'> location.href='../Base/inscription.php'</script>";
+        // }
+    }
+
+
 
 ?>
