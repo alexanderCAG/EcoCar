44c672f9ceaf3ae451e16b4dc6c7754291a97a70
alexanderCAG <61508790+alexanderCAG@users.noreply.github.com>
2022-02-26 22:27:19 +0100

    Update locationVoiture.php

--- Bdd/locationVoiture.php
+++ Bdd/locationVoiture.php
@@ -1,29 +1,6 @@
 
 <?php
 
-// if(isset($_GET['idVoiture']) && isset($_GET['idInscrit'])){
-//     include("cnx.php");
-//     session_start();
-
-//     $idVoiture=$_GET['idVoiture'];
-//     $idInscrit=$_GET['idInscrit'];
-
-//     $inscritAdmin = mysqli_query($con,"INSERT INTO admin (id_voiture_loue, id_inscription_loue, prix_voiture) 
-//                             VALUES ('$idVoiture', '$idInscrit', '369')");
-
-//     $recupInfoCompteur = mysqli_query($con, "SELECT compteur_location FROM voiture WHERE id='$idVoiture'");
-//     if($InfoCompteur = mysqli_fetch_assoc($recupInfoCompteur)){
-//         $compteur_location = $InfoCompteur['compteur_location'];
-//         $compteur_location ++ ;
-
-//         $updateVoiture = mysqli_query($con, "UPDATE voiture SET dispo='0', compteur_location='$compteur_location' WHERE id='$idVoiture'");
-
-
-//         echo "<script language='javascript' type='text/javascript'> location.href='../Acheteur' </script>";
-
-//     }
-// }
-
 if(isset($_POST['btnLocation'])){
 
     include("cnx.php");
