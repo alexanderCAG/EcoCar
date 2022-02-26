b68a29bcdc2a6e82787a638f9e9e58c0a9a4dd6f
alexanderCAG <61508790+alexanderCAG@users.noreply.github.com>
2022-02-26 19:24:20 +0100

    location voiture + promo calcul

--- Acheteur/detail_voiture.php
+++ Acheteur/detail_voiture.php
@@ -20,6 +20,7 @@
             $image= $infoVoitureTot['image'];
             $modele= $infoVoitureTot['modele'];
             $id_marque= $infoVoitureTot['id_marque'];
+            $promotion= $infoVoitureTot['promotion'];
             if($infoModelTot = mysqli_fetch_assoc($infoModel)){
                 $autonomie= $infoModelTot['autonomie'];   
                 $puissance= $infoModelTot['puissance'];   
@@ -31,12 +32,89 @@
                     $infoMarque = mysqli_query($con, "SELECT * FROM `marque` WHERE id='$id_marque'");
                     if($infoMarqueTot = mysqli_fetch_assoc($infoMarque)){
                         $marque= $infoMarqueTot['marque'];
+
+                        $infoPrix = mysqli_query($con, "SELECT * FROM `prix` WHERE id='$idVoiture'");
+                        if($infoPrixTot = mysqli_fetch_assoc($infoPrix)){
+                            $prixJ= $infoPrixTot['prixJour'];
+                            $prixS= $infoPrixTot['prixSemaine'];
+                            $prixM= $infoPrixTot['prixMois'];
     ?>
         <div class="col-6">
             <div class="info_nom"><p><b><?php echo $marque ?> &nbsp; <?php echo $modele ?></b></p></div>
             <img src="<?php echo $image ?>" alt="img_peugeot_e-208_bleu" class="img_detail_voiture">
             <br><br><br>
-            <a type="button" href="../Bdd/locationVoiture.php?idVoiture=<?php echo $idVoiture ?>&amp;idInscrit=<?php echo $idInscription ?>" class="btn_vert30 btn_detail">Louer celle-ci</a>
+            <!-- Button trigger modal -->
+            <a class="btn_vert30 btn_presention text-uppercase" data-bs-toggle="modal" data-bs-target="#exampleModal" style="cursor:pointer;">Réserver</a>
+
+            
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+                <div class="modal-dialog" >
+                    <div class="modal-content" style="width: 800px;left: -123px;top: 37px;" >
+                        <div class="modal-header">
+                            <h4 class="titre">Prix de la location </h4>
+                            <?php if($promotion!=0){ ?> 
+                            &nbsp; | &nbsp;
+                            <small><i> Produit en promotion </i><span class="badge bg-warning text-dark"><?php echo $promotion ?>%</span></small>
+                            <?php } ?>
+                            <hr>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <form action="../Bdd/locationVoiture.php" method="POST">
+                            <div class="modal-body">
+                                <div class="card shadow mt-2 p-3 py-5">
+                                    
+                                    <div class="row m-auto w-100">
+                                        <div class="col-md-4">
+                                            <div class="badge rounded-pill w-100 text-uppercase bg_green1 p-3">
+                                                <a class="h6 text-light text-decoration-none titre"> <i class="fas fa-2x fa-sun"></i> Prix journalier</a>
+                                            </div>
+                                            <center><i class="fas fa-caret-down fa-2x color_green1"></i></center>
+                                            <h3 class="titre text-center text-muted"> <?php echo $prixJ ?> €</h3>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="badge rounded-pill w-100 text-uppercase bg_green1 p-3">
+                                                <a class="h6 text-light text-decoration-none titre"> <i class="fas fa-2x fa-calendar-day"></i> Prix hebdomadaire</a>
+                                            </div>
+                                            <center><i class="fas fa-caret-down fa-2x color_green1"></i></center>
+                                            <h3 class="titre text-center text-muted"> <?php echo $prixS ?> €</h3>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="badge rounded-pill w-100 text-uppercase bg_green1 p-3">
+                                                <a class="h6 text-light text-decoration-none titre"> <i class="fas fa-2x fa-calendar-day"></i> Prix mensuel</a>
+                                            </div>
+                                            <center><i class="fas fa-caret-down fa-2x color_green1"></i></center>
+                                            <h3 class="titre text-center text-muted"> <?php echo $prixM ?> €</h3>
+                                        </div>
+                                    </div>
+                                    <div class="row g-3 align-items-center mt-4 ">
+                                        <div class="col-md-6">
+                                            <span id="passwordHelpInline" class="form-text">
+                                            Date de début 
+                                            </span>
+                                            <input type="date" name="dateDebut" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="passwordHelpInline" class="form-text">
+                                            Date de Fin 
+                                            </span>
+                                            <input type="date" name="dateFin" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+                                        </div>
+                                        <input type="hidden" name="idVoiture" value="<?php echo $idVoiture ?>">
+                                        <input type="hidden" name="idInscription" value="<?php echo $idInscription ?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" name="btnLocation" class="btn_vert10  w-100 border-0" >Envoyer</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- <a type="button" href="../Bdd/locationVoiture.php?idVoiture=<?php echo $idVoiture ?>&amp;idInscrit=<?php echo $idInscription ?>" class="btn_vert30 btn_detail">Louer celle-ci</a> -->
         </div>
 
         <div class="col-1 droite_detail"></div>
@@ -101,6 +179,7 @@
                     </div>
                 </div>
             <?php
+                                }
                             }
                         }
                     }
--- Bdd/locationVoiture.php
+++ Bdd/locationVoiture.php
@@ -1,26 +1,120 @@
 
 <?php
 
-if(isset($_GET['idVoiture']) && isset($_GET['idInscrit'])){
+// if(isset($_GET['idVoiture']) && isset($_GET['idInscrit'])){
+//     include("cnx.php");
+//     session_start();
+
+//     $idVoiture=$_GET['idVoiture'];
+//     $idInscrit=$_GET['idInscrit'];
+
+//     $inscritAdmin = mysqli_query($con,"INSERT INTO admin (id_voiture_loue, id_inscription_loue, prix_voiture) 
+//                             VALUES ('$idVoiture', '$idInscrit', '369')");
+
+//     $recupInfoCompteur = mysqli_query($con, "SELECT compteur_location FROM voiture WHERE id='$idVoiture'");
+//     if($InfoCompteur = mysqli_fetch_assoc($recupInfoCompteur)){
+//         $compteur_location = $InfoCompteur['compteur_location'];
+//         $compteur_location ++ ;
+
+//         $updateVoiture = mysqli_query($con, "UPDATE voiture SET dispo='0', compteur_location='$compteur_location' WHERE id='$idVoiture'");
+
+
+//         echo "<script language='javascript' type='text/javascript'> location.href='../Acheteur' </script>";
+
+//     }
+// }
+
+if(isset($_POST['btnLocation'])){
+
     include("cnx.php");
     session_start();
 
-    $idVoiture=$_GET['idVoiture'];
-    $idInscrit=$_GET['idInscrit'];
+    $dateDebut=$_POST['dateDebut'];
+    $dateFin=$_POST['dateFin'];
+    $idVoiture=$_POST['idVoiture'];
+    $idInscrit=$_POST['idInscription'];
 
-    $inscritAdmin = mysqli_query($con,"INSERT INTO admin (id_voiture_loue, id_inscription_loue, prix_voiture) 
-                            VALUES ('$idVoiture', '$idInscrit', '369')");
+    // $jour = $dateFin->diff($dateDebut)->format('%d');
+    $jour = strtotime($dateFin) - strtotime($dateDebut);
 
-    $recupInfoCompteur = mysqli_query($con, "SELECT compteur_location FROM voiture WHERE id='$idVoiture'");
-    if($InfoCompteur = mysqli_fetch_assoc($recupInfoCompteur)){
-        $compteur_location = $InfoCompteur['compteur_location'];
-        $compteur_location ++ ;
+    var_dump(strtotime($dateFin));
+    var_dump(strtotime($dateDebut));
 
-        $updateVoiture = mysqli_query($con, "UPDATE voiture SET dispo='0', compteur_location='$compteur_location' WHERE id='$idVoiture'");
+    $jour = $jour / 86400;
+    var_dump($jour);
 
+    $recupPrix = mysqli_query($con, "SELECT * FROM prix WHERE id='$idVoiture'");
+    if($InfoRecupPrix = mysqli_fetch_assoc($recupPrix)){
+        $prixJour = $InfoRecupPrix['prixJour'];
+        $prixSemaine = $InfoRecupPrix['prixSemaine'];
+        $prixMois = $InfoRecupPrix['prixMois'];
 
-        echo "<script language='javascript' type='text/javascript'> location.href='../Acheteur' </script>";
+        $recupInfoCompteur = mysqli_query($con, "SELECT * FROM voiture WHERE id='$idVoiture'");
+        if($InfoCompteur = mysqli_fetch_assoc($recupInfoCompteur)){
+            $compteur_location = $InfoCompteur['compteur_location'];
+            $promotion = $InfoCompteur['promotion'];
+            $compteur_location ++ ;
 
+            if($promotion!=0){
+                // Jour
+                if($jour>0 && $jour<=7){
+                    $calculPrix = ($jour * $prixJour) - ($jour * $prixJour * ($promotion/100));
+    
+                    $inscritAdmin = mysqli_query($con,"INSERT INTO admin (id_voiture_loue, id_inscription_loue, prix_voiture) VALUES ('$idVoiture', '$idInscrit', '$calculPrix')");
+                    $updateVoiture = mysqli_query($con, "UPDATE voiture SET dispo='0', compteur_location='$compteur_location' WHERE id='$idVoiture'");
+                    // echo "<script language='javascript' type='text/javascript'> location.href='../Acheteur' </script>";
+                }
+                // Semaine
+                else if($jour>7 && $jour<=28){
+                    $jour = $jour / 7;
+                    $calculPrix = ($jour * $prixSemaine) - ($jour * $prixJour * ($promotion/100));
+    
+                    $inscritAdmin = mysqli_query($con,"INSERT INTO admin (id_voiture_loue, id_inscription_loue, prix_voiture) VALUES ('$idVoiture', '$idInscrit', '$calculPrix')");
+                    $updateVoiture = mysqli_query($con, "UPDATE voiture SET dispo='0', compteur_location='$compteur_location' WHERE id='$idVoiture'");
+                    // echo "<script language='javascript' type='text/javascript'> location.href='../Acheteur' </script>";
+                }
+                // Mois
+                else if($jour>28){
+                    $jour = $jour / 28;
+                    $calculPrix = ($jour * $prixMois) - ($jour * $prixJour * ($promotion/100));
+    
+                    $inscritAdmin = mysqli_query($con,"INSERT INTO admin (id_voiture_loue, id_inscription_loue, prix_voiture) VALUES ('$idVoiture', '$idInscrit', '$calculPrix')");
+                    $updateVoiture = mysqli_query($con, "UPDATE voiture SET dispo='0', compteur_location='$compteur_location' WHERE id='$idVoiture'");
+                    // echo "<script language='javascript' type='text/javascript'> location.href='../Acheteur' </script>";
+                }
+            }else{
+                // Jour
+                if($jour>0 && $jour<=7){
+                    $calculPrix = $jour * $prixJour;
+    
+                    $inscritAdmin = mysqli_query($con,"INSERT INTO admin (id_voiture_loue, id_inscription_loue, prix_voiture) VALUES ('$idVoiture', '$idInscrit', '$calculPrix')");
+                    $updateVoiture = mysqli_query($con, "UPDATE voiture SET dispo='0', compteur_location='$compteur_location' WHERE id='$idVoiture'");
+                    // echo "<script language='javascript' type='text/javascript'> location.href='../Acheteur' </script>";
+                }
+                // Semaine
+                else if($jour>7 && $jour<=28){
+                    $jour = $jour / 7;
+                    $calculPrix = $jour * $prixSemaine;
+    
+                    $inscritAdmin = mysqli_query($con,"INSERT INTO admin (id_voiture_loue, id_inscription_loue, prix_voiture) VALUES ('$idVoiture', '$idInscrit', '$calculPrix')");
+                    $updateVoiture = mysqli_query($con, "UPDATE voiture SET dispo='0', compteur_location='$compteur_location' WHERE id='$idVoiture'");
+                    // echo "<script language='javascript' type='text/javascript'> location.href='../Acheteur' </script>";
+                }
+                // Mois
+                else if($jour>28){
+                    $jour = $jour / 28;
+                    $calculPrix = $jour * $prixMois;
+    
+                    $inscritAdmin = mysqli_query($con,"INSERT INTO admin (id_voiture_loue, id_inscription_loue, prix_voiture) VALUES ('$idVoiture', '$idInscrit', '$calculPrix')");
+                    $updateVoiture = mysqli_query($con, "UPDATE voiture SET dispo='0', compteur_location='$compteur_location' WHERE id='$idVoiture'");
+                    // echo "<script language='javascript' type='text/javascript'> location.href='../Acheteur' </script>";
+                }
+            }
+
+            
+
+        
+        }
     }
 }
 
