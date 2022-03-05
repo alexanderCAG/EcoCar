6e3a0e96437d9245a7cdc75b087966d7cc64a85a
alexanderCAG <61508790+alexanderCAG@users.noreply.github.com>
2022-03-05 10:15:22 +0100

    Update liste_marque.php

--- Admin/liste_marque.php
+++ Admin/liste_marque.php
@@ -1,4 +1,8 @@
-<?php require('header.php'); ?>
+<?php 
+    require('header.php');
+
+    $totalMarque = mysqli_query($con, "SELECT count(DISTINCT(marque)) as total_marque FROM `marque`");
+?>
 
 <div class="total_categorie">
 
@@ -16,7 +20,39 @@
 
         <!-- Partie Centrale contenu -->
         <section>
-            <table class="table table_marque">
+            <div class="row mx-2 mt-3">
+            <?php
+                        if (isset($_GET['page_no']) && $_GET['page_no']!="") {
+                        $page_no = $_GET['page_no'];
+                        } else {
+                        $page_no = 1;
+                        }
+
+                        $total_records_per_page = 12;
+                        $offset = ($page_no-1) * $total_records_per_page;
+                        $previous_page = $page_no - 1;
+                        $next_page = $page_no + 1;
+                        $adjacents = "2"; 
+
+                        $result_count = mysqli_query($con,"SELECT COUNT(DISTINCT(marque)) As total_records FROM `marque`");
+                        $total_records = mysqli_fetch_array($result_count);
+                        $total_records = $total_records['total_records'];
+                        $total_no_of_pages = ceil($total_records / $total_records_per_page);
+                        $second_last = $total_no_of_pages - 1; // total page minus 1
+
+                        $result = mysqli_query($con,"SELECT * FROM `marque` LIMIT $offset, $total_records_per_page ");
+                        while($afficheTot = mysqli_fetch_array($result)){
+                            $logoMarque= $afficheTot['logo'];
+
+                    ?>
+                <div class="col col-4 mb-5 px-2" style="height: 300px">
+                    <img src="<?= $logoMarque ?>" alt="" width="100%" height="300px">
+                </div>
+                <?php
+                    }
+                ?>
+            </div>
+            <!-- <table class="table table_marque">
                 <tbody>
                     <tr>
                         <td class="img_marque_liste" rowspan="2" colspan="2"></td>
@@ -39,24 +75,29 @@
                         <td class="img_marque_liste" colspan="3"></td>
                     </tr>
                 </tbody>
-            </table>
+            </table> -->
         </section>
 
         <!-- Pagination -->
-        <section style="margin-top:40px; margin-bottom:37px;">
-            <nav aria-label="Page navigation example">
+        <section style="margin-top:100px; margin-bottom:90px;">
+            <nav aria-label="Page navigation example">                
                 <ul class="pagination justify-content-center">
-                    <li class="page-item disabled">
-                        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                    <?php
+                        if($page_no > 1){ echo "<li class='page-item'><a class='page-link' href='?page_no=1'> &#139;&#139; First Page</a></li>"; } 
+                    ?>
+                    
+                    <li class="page-item" <?php if($page_no <= 1){ echo "class='disabled page-item'"; } ?>>
+                        <a class="page-link" <?php if($page_no > 1){ echo "href='?page_no=$previous_page'"; } ?>>Previous</a>
                     </li>
 
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <?php require('pagination.php');?>
 
-                    <li class="page-item">
-                        <a class="page-link" href="#">Next</a>
+                    <li class='page-item' <?php if($page_no >= $total_no_of_pages){ echo "class='disabled page-item'"; } ?>>
+                        <a class='page-link' <?php if($page_no < $total_no_of_pages) { echo "href='?page_no=$next_page'"; } ?>>Next</a>
                     </li>
+                    <?php if($page_no < $total_no_of_pages){
+                        echo "<li class='page-item'><a class='page-link' href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+                    } ?>
                 </ul>
             </nav>
         </section>
